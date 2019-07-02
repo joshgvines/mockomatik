f400dee81f2e566e948d90ee9fb44839808cb312
joshgvines <joshgvines@gmail.com>
2019-07-01 20:15:32 -0500

    adding gitignore

--- src/com/application/Application.java
+++ src/com/application/Application.java
@@ -7,13 +7,6 @@ import java.nio.file.Path;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("_________________________________________________________________");
-        System.out.println(" _______________________________________________________________ ");
-        System.out.println("|_   _| ____/ ___|_   _|  / _ \\( |  \\/  |  / \\|_   _|_ _/ ___|||");
-        System.out.println("  | | |  _| \\___ \\ | |   | | | |/| |\\/| | / _ \\ | |  | | |    ");
-        System.out.println("  | | | |___ ___) || |   | |_| | | |  | |/ ___ \\| |  | | |______ ");
-        System.out.println("  |_| |_____|____/ |_|    \\___/  |_|  |_/_/   \\_|_| |___\\____|||");
-        System.out.println("_________________________________________________________________");
 
         Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
         System.out.println("Your Location: " + path);
@@ -21,7 +14,7 @@ public class Application {
             InputController inputController = new InputController();
             inputController.runProgram();
         } else {
-            System.out.println("Do not run in root drive!");
+            System.out.println("Do not run in root drive");
         }
     }
 }
