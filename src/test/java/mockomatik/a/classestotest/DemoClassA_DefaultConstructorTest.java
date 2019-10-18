82f4bb2137058506d44e9284b2c57a4bb8a1916b
josh <joshgvines@gmail.com>
2019-10-18 13:34:09 -0500

    gradle and gitignore config changes

---
+++ src/test/java/mockomatik/a/classestotest/DemoClassA_DefaultConstructorTest.java
@@ -0,0 +1,31 @@
+package mockomatik.a.classestotest; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DemoClassA_DefaultConstructorTest {
+
+	private DemoClassA_DefaultConstructor cut;
+
+	@Before
+	public void setUp() {
+		cut = new DemoClassA_DefaultConstructor(// Ignored
+
+		);
+	}
+
+	@After
+	public void tearDown() {
+		cut = null;
+	}
+
+	@Test
+	public void testMethod() {
+		//blah
+	}
+}
---
+++ src/test/java/mockomatik/classes/service/create/CreateTestClassesTest.java
@@ -0,0 +1,12 @@
+package mockomatik.classes.service.create;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreateTestClassesTest {
+
+    @Test
+    public void createTest() {
+    }
+}
